f5ef45a158110a5834277ad045bd3cce966208ff
IronLegend000 <romeromucel@gmail.com>
2024-10-29 16:33:35 -0600

    hero section modify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
   return (
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px} w--full h-screen mx-auto text-center flex flex-col justify-center' >
-          <p className='text-[#00df9a] font-bold p-2'> GROWING WITH DATA ANALYTICS</p>
+          <p className='text-[#00df9a] font-bold p-2'> GROWING WITH DATA ANALYTIC</p>
           <h1 className='md:text-7xl sm:text-xl text-4xl font-bold md:mpy-6 '>Growing with data.</h1>
           <div>
             <p className='md:text-5xl sm:text-4xl text-xl font-bold '>Fast, Flexible financing for </p>
