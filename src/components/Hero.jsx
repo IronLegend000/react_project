1beb782fcdd7d101a50ff7f8bd93468c9a78aa1b
nellyherprec <nellyherprec@MacBook-Pro-de-nellyherprec.local>
2024-10-29 17:13:04 -0600

    fix trashy code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Typed  from "react-typed";
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 
 export const Hero = () => {
   return (
     <div className='text-white'>
-        <div className='max-w-[800px] mt-[-96px} w--full h-screen mx-auto text-center flex flex-col justify-center' >
-          <p className='text-[#00df9a] font-bold p-2'> GROWING WITH DATA ANALYTIC</p>
-          <h1 className='md:text-7xl sm:text-xl text-4xl font-bold md:mpy-6 '>Growing with data.</h1>
-          <div>
-            <p className='md:text-5xl sm:text-4xl text-xl font-bold '>Fast, Flexible financing for </p>
-            <Typed strings={['BTB','BTC', 'SASS']} 
-            typedSpeed={120} 
-            backSpeed={140}
-            loop />          
-          </div>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+        <h1 className='md:text-7xl sm:text-4xl text-4xl font-bold md:my-6'>Growing with data.</h1>
+        <div>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>
+            Fast, Flexible financing for{' '}
+            <span className='text-[#00df9a]'>
+              <Typewriter words={['Startups', 'Businesses', 'Enterprises']} loop cursor />
+            </span>
+          </p>
         </div>
+      </div>
     </div>
   );
 };
