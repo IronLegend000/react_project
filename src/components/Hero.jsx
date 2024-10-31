1520bfafaabadecec7a8def6e5a8370915a6b7d5
IronLegend000 <romeromucel@gmail.com>
2024-10-31 17:52:08 -0600

    new update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,14 @@ export const Hero = () => {
         <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
         <h1 className='md:text-7xl sm:text-4xl text-4xl font-bold md:my-6'>Growing with data.</h1>
         <div>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'>
             Fast, Flexible financing for{' '}
             <span className='text-[#00df9a]'>
               <Typewriter words={['Startups', 'Businesses', 'Enterprises']} loop cursor />
             </span>
           </p>
         </div>
+        <p className='md:text2xl text-xl font-bold text-gray-500'> Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms. </p>
       </div>
     </div>
   );
